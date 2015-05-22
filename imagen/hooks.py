ae28561fec95524e3b60fc6a6e8b3d68e05b4fd2
David Goyard <david.goyard@cea.fr>
2015-05-22 10:55:46 +0200

    cubes/imagen/hooks: add hooks to ease LDAP synchronisation (group entities), pre-allocate the cache import/imagen/a_import_imagen: pep8 corrections and remove deprecated parts (can't remove user groups from here) import/imagen/imagen_fs_parser: add questionnaires origins (DAWBA) in questionnaire names

--- imagen/hooks.py
+++ imagen/hooks.py
@@ -7,3 +7,30 @@
 # for details.
 ##########################################################################
 
+"""cubicweb-imagen specific hooks and operations"""
+
+from cubicweb.server.hook import Hook
+
+class ServerStartupHook(Hook):
+    """
+        Update repository cache with groups from indexation to ease LDAP
+        synchronisation
+    """
+    __regid__ = 'imagen.update_cache_hook'
+    events = ('server_startup', 'server_maintenance')
+
+    def __call__(self):
+        # get session
+
+        # update repository cache
+        print(self.repo._extid_cache)
+        with self.repo.internal_cnx() as cnx:
+            rset = cnx.execute("Any X WHERE X is CWGroup")
+            for egroup in rset.entities():
+                if egroup.name in ["guests", "managers", "users", "owners"]:
+                    continue
+                self.repo._extid_cache[
+                    'cn={0},ou=Groups,dc=imagen2,dc=cea,dc=fr'.format(
+                        egroup.name)] = egroup.eid
+        print(self.repo._extid_cache)
+
