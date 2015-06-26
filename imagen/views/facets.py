9dec946f1af746bcc971ccf0d33c3bb9a29ebae5
Robin CHERBONNIER <robin.cherbonnier@cea.fr>
2015-06-26 16:58:39 +0200

    cubes/imagen/views/facets.py : fix filter component removal during filering

--- imagen/views/facets.py
+++ imagen/views/facets.py
@@ -1,5 +1,28 @@
 from cubicweb.web.views.facets import FacetFilterMixIn
 
+## Hide filter component while filtering
 
-# Use of a cusom JS to freeze filters when one is being processed
-FacetFilterMixIn.needs_js = ['cubicweb.ajax.js', 'imagen.cubicweb.facets.js']
+FacetFilterMixIn._generate_form = FacetFilterMixIn.generate_form
+
+def generate_form(self, w, rset, divid, vid, vidargs=None, mainvar=None,
+                  paginate=False, cssclass='', hiddens=None, **kwargs):
+
+        FacetFilterMixIn._generate_form(self, w, rset, divid, vid, vidargs=None,
+                                        mainvar=None, paginate=False,
+                                        cssclass='', hiddens=None, **kwargs)
+
+        html = "<script>"
+        html += "function onFacetFiltering(event, divid /* ... */) {"
+        html += "$('#facet_filterbox').hide();"
+        html += "showFacetLoading(divid);"
+        html += "}"
+        html += ("function onFacetContentLoaded"
+                 "(event, divid, rql, vid, extraparams) {")
+        html += "$('#facetLoading').hide();"
+        html += "$('#facet_filterbox').show();"
+        html += "}"
+        html += "</script>"
+
+        w(u'{0}'.format(html))
+
+FacetFilterMixIn.generate_form = generate_form
